1fc4861ab120a5d6f305f3aa423680b4685c4e5b
Yuliya Chekan <juliabel5@gmail.com>
2023-01-22 15:36:28 +0300

    feat: add files

--- Plants/index.js
+++ Plants/index.js
@@ -1,1 +1,26 @@
-console.log('Hello world!');
+let hamburger = document.querySelector('.header-menu');
+let menu = document.querySelector('.burger__menu');
+hamburger.addEventListener('click', function(){
+	hamburger.classList.toggle('active');
+        menu.classList.toggle('active');
+})
+
+const link = document.querySelector('.burger-list')
+document.addEventListener( 'click', (e) => {
+	const linkClick = e.composedPath().includes(link);
+ 
+	if ( linkClick ) {
+		menu.classList.remove('active')
+		hamburger.classList.remove('active'); 
+	}
+})
+
+const popup = document.querySelector('.header__burger')
+document.addEventListener( 'click', (e) => {
+	const withinBoundaries = e.composedPath().includes(popup);
+ 
+	if ( ! withinBoundaries ) {
+		menu.classList.remove('active')
+		hamburger.classList.remove('active'); 
+	}
+})
